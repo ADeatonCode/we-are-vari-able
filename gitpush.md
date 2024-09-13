4634bcbee70a04332cc1afa19554b628f6f090c4
Alan <acdelgato@gmail.com>
2024-09-12 20:53:02 -0500

    After test passed

---
+++ gitpush.md
@@ -0,0 +1,7 @@
+echo "# we-are-vari-able" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/ADeatonCode/we-are-vari-able.git
+git push -u origin main
---
+++ main.js
@@ -0,0 +1,98 @@
+/******************
+ * YOUR CODE BELOW *
+ ******************/
+let myFirstName = "Alan";
+let myLastName = "Deaton";
+let myFavoriteCeleb = "David Crosby";
+let mySentence = "I'm " + myFirstName + " " + myLastName + ", a software engineer with a passion for music by " + myFavoriteCeleb + ".";
+let myLuckyNum = 1234;
+let myAge = 58;
+let myHeight = 69;
+let myFavoriteYear = 1969;
+let mySum = 5 + 2317 + 36
+let myProduct = 232 * 43 / 2+4;
+let myHeightAndAge = myAge + myHeight;
+
+/*********************************************************************************************************************
+
+ * CODE BELOW HERE IS FOR INTERNAL USE ONLY! IT ENSURES THAT THE TESTING *
+ * CAN READ AND EVALUATE YOUR CODE. *
+ * DON'T CHANGE ANYTHING BELOW HERE, PLEASE! *
+
+ *********************************************************************************************************************/
+
+if (typeof myFirstName === "undefined") {
+  myFirstName = undefined;
+}
+
+if (typeof myLastName === "undefined") {
+  myLastName = undefined;
+}
+
+if (typeof myFavoriteCeleb === "undefined") {
+  myFavoriteCeleb = undefined;
+}
+
+if (typeof mySentence === "undefined") {
+  mySentence = undefined;
+}
+
+if (typeof myLuckyNum === "undefined") {
+  myLuckyNum = undefined;
+}
+
+if (typeof myAge === "undefined") {
+  myMentalAge = undefined;
+}
+
+if (typeof myHeight === "undefined") {
+  myHeight = undefined;
+}
+
+if (typeof myFavoriteYear === "undefined") {
+  myFavoriteYear = undefined;
+}
+
+if (typeof mySum === "undefined") {
+  mySum = undefined;
+}
+
+if (typeof myProduct === "undefined") {
+  myProduct = undefined;
+}
+
+if (typeof myHeightAndAge === "undefined") {
+  myHeightAndAge = undefined;
+}
+
+module.exports = {
+  myFirstName,
+  myLastName,
+  myFavoriteCeleb,
+  mySentence,
+  myLuckyNum,
+  myAge,
+  myHeight,
+  myFavoriteYear,
+  mySum,
+  myProduct,
+  myHeightAndAge,
+};
+
+//### Tasks
+
+//- Create a variable called `myFirstName`. Assign `myFirstName` a string value that represents your first name.
+//- Create a variable called `myLastName`. Assign `myLastName` a string value that represents your last name.
+//- Create a variable called `myFavoriteCeleb`. Assign `myFavoriteCeleb` a string value that represents your favorite Celebrity's name.
+//- Create a variable called `mySentence`. Assign `mySentence` a string that includes the variables from the previous three tasks.
+//- Create another variable called `myLuckyNum`. Assign `myLuckyNum` a number value that represents your lucky number!
+//- Create another variable called `myAge`. Assign `myAge` a number value that represents your age!
+//- Create another variable called `myHeight`. Assign `myHeight` a number value that represents your height in inches or centimeters!
+//- Create another variable called `myFavoriteYear`. Assign `myFavoriteYear` a number value that represents your favorite year!
+
+//### Tasks that use MATH
+
+//- Create another variable called `mySum`. Set it equal to the result of five plus two thousand three hundred seventeen plus thirty-six. Calculate it in code!
+//- Create another variable called `myProduct`. Set it equal to the result of two hundred thirty-two times forty-three divided by two plus four (the operations should be written in that order!). Calculate it in code!
+//- Create another variable called `myHeightAndAge`. Set it equal to the sum of your age and height. Calculate it in code!
+//
